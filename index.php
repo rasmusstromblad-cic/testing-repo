63f28721ad49a8012e6fbdad81e7234009c1a16b
Rasmus Strömblad <rasmusstromblad@Rasmuss-MacBook-Pro.local>
2023-05-25 14:13:37 +0200

    Test code to trigger cluster

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World! This is a new version of the app. The php version is " . PHP_VERSION . "\n";
+print "Hello, World! This is a message that proves the S2I capabilities The php version is " . PHP_VERSION . "\n";
 ?>
