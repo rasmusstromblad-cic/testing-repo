28e52c91264cdad5903d78cff4b18c64478206cc
Rasmus Strömblad <rasmusstromblad@Rasmuss-MacBook-Pro.local>
2023-05-25 14:39:08 +0200

    Test code

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World! This is a message that proves the capabilities of S2I. Is this working? Either way, the php version is " . PHP_VERSION . "\n";
+print "Hello, World! This is a message that proves the capabilities of S2I. The question is, is it working? Will we ever know? Either way, the php version is " . PHP_VERSION . "\n";
 ?>
