d6c30f100662a068d5bbb12d13286de42c8968fb
rasmusstromblad-cic <131746425+rasmusstromblad-cic@users.noreply.github.com>
2023-06-20 09:25:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Welcome to this app")
+from flask import Flask  # From module flask import class Flask
+app = Flask(__name__)    # Construct an instance of Flask class for our webapp
+
+@app.route('/')   # URL '/' to be handled by main() route handler
+def main():
+    """Say hello"""
+    return 'Hello, world!'
+
+if __name__ == '__main__':  # Script executed directly?
+    print("Hello, World. Uses S2I to build the application.")
+    app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)
